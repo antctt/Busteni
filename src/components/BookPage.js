0f3e06afd0aa2997ac10097abc680626604cd5ef
antctt <tonicorentti@gmail.com>
2023-04-11 18:01:04 +0300

    update BookPage.js

--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Button, Row, Col, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
-
+import React, { useState, useEffect } from "react";
+import { Container, Button, Row, Col, Form } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 
 const BookPage = () => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [disabledDates, setDisabledDates] = useState([]);
+  const [nextAvailableDate, setNextAvailableDate] = useState(new Date());
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -22,6 +23,33 @@ const BookPage = () => {
     });
 
     return () => unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      const db = getFirestore();
+      const bookingsRef = collection(db, "Bookings");
+      const allBookingsSnapshot = await getDocs(bookingsRef);
+      const allBookings = allBookingsSnapshot.docs.map((doc) => doc.data());
+
+      const disabledDates = [];
+      allBookings.forEach((booking) => {
+        const bookingStartDate = new Date(booking.startDate);
+        const bookingEndDate = new Date(booking.endDate);
+
+        for (
+          let d = bookingStartDate;
+          d <= bookingEndDate;
+          d.setDate(d.getDate() + 1)
+        ) {
+          disabledDates.push(new Date(d));
+        }
+      });
+
+      setDisabledDates(disabledDates);
+    };
+
+    fetchBookings();
   }, []);
 
   const handleBookNow = async () => {
@@ -36,6 +64,7 @@ const BookPage = () => {
     });
   };
 
+
   return (
     <Container className="mt-5">
       {loading ? (
@@ -45,12 +74,12 @@ const BookPage = () => {
           <h2>Please Log In or Sign Up to book a house</h2>
           <Row className="justify-content-center my-5">
             <Col xs="auto">
-              <Button variant="primary" onClick={() => navigate('/login')}>
+              <Button variant="primary" onClick={() => navigate("/login")}>
                 Log In
               </Button>
             </Col>
             <Col xs="auto">
-              <Button variant="secondary" onClick={() => navigate('/signup')}>
+              <Button variant="secondary" onClick={() => navigate("/signup")}>
                 Sign Up
               </Button>
             </Col>
@@ -66,6 +95,9 @@ const BookPage = () => {
                 selected={startDate}
                 onChange={(date) => setStartDate(date)}
                 className="form-control"
+                excludeDates={disabledDates}
+                minDate={new Date()}
+                openToDate={nextAvailableDate}
               />
             </Form.Group>
             <Form.Group className="mb-3">
@@ -74,8 +106,12 @@ const BookPage = () => {
                 selected={endDate}
                 onChange={(date) => setEndDate(date)}
                 className="form-control"
+                excludeDates={disabledDates}
+                minDate={startDate}
+                openToDate={nextAvailableDate}
               />
             </Form.Group>
+
             <Button variant="primary" onClick={handleBookNow}>
               Book Now
             </Button>
