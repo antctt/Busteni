cd1f6107064ada9f0a28c01f63fa9b1de3e1cbe3
Tony <corentti.a@pg.com>
2023-05-25 16:44:39 +0300

    broken update

--- src/components/GoTogether.js
+++ src/components/GoTogether.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { auth, db } from '../firebase';
 import './WeekendEvents.css';
+import { getFirestore, addDoc } from 'firebase/firestore';
 
 const WeekendEvents = () => {
   const [weekends, setWeekends] = useState([]);
   const [user, setUser] = useState(null);
   const [bookings, setBookings] = useState([]);
+
+  const maxSpots = 12;
 
   useEffect(() => {
     const getRemainingWeekends = () => {
@@ -15,7 +18,12 @@ const WeekendEvents = () => {
       const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 2, 0);
       let current = new Date(now.getFullYear(), now.getMonth(), now.getDate());
       const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  
+    
+      // If it's already Friday, Saturday or Sunday, start from the next Friday
+      if (current.getDay() >= 5) {
+        current.setDate(current.getDate() + (7 - current.getDay() + 5));
+      }
+    
       while (current < endOfMonth && weekends.length < 4) {
         if (current.getDay() === 5) {
           const sunday = new Date(current.getFullYear(), current.getMonth(), current.getDate() + 2);
@@ -31,6 +39,7 @@ const WeekendEvents = () => {
       }
       return weekends;
     };
+    
 
     const fetchBookingsFromFirestore = async () => {
       const snapshot = await db.collection('Bookings').get();
@@ -56,8 +65,17 @@ const WeekendEvents = () => {
   
     const fetchWeekendsFromFirestore = async () => {
       const snapshot = await db.collection('weekends').get();
-      const weekends = await Promise.all(snapshot.docs.map(async (doc) => {
+      let weekends = await Promise.all(snapshot.docs.map(async (doc) => {
         const weekend = { id: doc.id, ...doc.data() };
+        const now = new Date();
+        const weekendStartDate = new Date(weekend.id.split('-'));
+    
+        // If the weekend has passed, delete it from the database
+        if (weekendStartDate < now) {
+          await db.collection('weekends').doc(weekend.id).delete();
+          return null;
+        }
+    
         if (weekend.joinedUsers) {
           weekend.joinedUsers = await Promise.all(weekend.joinedUsers.map(async (userId) => {
             const userDoc = await db.collection('users').doc(userId).get();
@@ -66,14 +84,22 @@ const WeekendEvents = () => {
         }
         return weekend;
       }));
+    
+      // Filter out null values (deleted weekends) and sort by date
+      weekends = weekends.filter(Boolean).sort((a, b) => {
+        const aDate = new Date(a.id.split('-'));
+        const bDate = new Date(b.id.split('-'));
+        return aDate - bDate; // Ascending order
+      });
+    
       setWeekends(weekends);
     };
-      
+              
     const remainingWeekends = getRemainingWeekends();
     addWeekendsToFirestore(remainingWeekends);
     fetchWeekendsFromFirestore();
   
-    setWeekends(getRemainingWeekends());
+    // setWeekends(getRemainingWeekends());
 
     const unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -84,7 +110,15 @@ const WeekendEvents = () => {
       }
     });
     const unsubscribeFromWeekends = db.collection('weekends').onSnapshot((snapshot) => {
-      const newWeekends = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      let newWeekends = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      
+      // Sort newWeekends before setting them to state
+      newWeekends = newWeekends.sort((a, b) => {
+        const aDate = new Date(a.id.split('-'));
+        const bDate = new Date(b.id.split('-'));
+        return aDate - bDate; // Ascending order
+      });
+      
       setWeekends(newWeekends);
     });
 
@@ -95,14 +129,40 @@ const WeekendEvents = () => {
 
   }, []);
 
-  const isWeekendBooked = (weekend) => {
-    return bookings.some((booking) => {
-      const weekendStart = new Date(weekend.id.split('-'));
-      const weekendEnd = new Date(weekend.id.split('-'));
-      weekendEnd.setDate(weekendEnd.getDate() + 2);
-      return booking.startDate <= weekendEnd && booking.endDate >= weekendStart;
+  const isWeekendBooked = (weekendId) => {
+    console.log(`Checking if weekend ${weekendId} is booked...`);
+
+    // Convert weekendId to a Date object
+    const weekendDate = new Date(weekendId);
+
+    const booking = bookings.find((booking) => {
+        const bookingStartDate = new Date(booking.startDate);
+        return bookingStartDate.getFullYear() === weekendDate.getFullYear() &&
+               bookingStartDate.getMonth() === weekendDate.getMonth() &&
+               bookingStartDate.getDate() === weekendDate.getDate();
     });
-  };
+
+    console.log(`Found booking: `, booking);
+
+    if (!booking) {
+      return false;
+    }
+
+    // Check if the booking has multiple users
+    const users = booking.userName.split(', ');
+    console.log(`Users: `, users);
+
+    if (users.length > 1) {
+      // If it has multiple users, only mark it as booked if it has 12 users
+      return users.length >= 12;
+    }
+
+    // If it has only one user, mark it as booked directly
+    return true;
+};
+
+
+  
 
   const handleJoin = async (weekendId) => {
     if (!user) {
@@ -126,6 +186,18 @@ const WeekendEvents = () => {
         }
       });
       setWeekends(newWeekends);
+      
+      // If 2 or more people have joined, add a booking
+      if (joinedUsers.length >= 2) {
+        const startDate = new Date(weekendId.split('-'));
+        const endDate = new Date(weekendId.split('-'));
+        endDate.setDate(endDate.getDate() + 2);
+        await db.collection('Bookings').add({
+          userName: joinedUsers.map(joinedUser => joinedUser.email).join(', '),
+          startDate: startDate.toISOString(),
+          endDate: endDate.toISOString(),
+        });
+      }
     }
   };
   
@@ -176,52 +248,55 @@ const WeekendEvents = () => {
         GoTogether is a brand new feature that allows you to meet new people and go on adventures together. Join a group of people and go on a weekend trip to a new city. <br />
         You can also confirm your attendance to a trip that you have already joined. You can only join one trip at a time, so make sure you confirm your attendance before joining another trip.
       </p>
-      {weekends.map((weekend) => (
-        <div key={weekend.id} className="weekend-container" style={{ height: isWeekendBooked(weekend) ? "70px" : "normalHeight" }}>
-        <div className="header">
-          <h3>{weekend.dateRange}
-          {isWeekendBooked(weekend) && <span style={{ color: 'green' }}> (Booked)</span>}</h3>
-          <div className="buttons">
-          <Button 
-            variant="success" 
-            className="join-btn" 
-            onClick={() => handleJoin(weekend.id)}
-            disabled={isWeekendBooked(weekend)} // Add this line
-          >
-            Join
-          </Button>
-
-          <Button 
-            variant="danger" 
-            className="leave-btn" 
-            onClick={() => handleLeave(weekend.id)}
-            disabled={isWeekendBooked(weekend)} // Add this line
-          >
-            Leave
-          </Button>
+      {weekends.map((weekend) => {
+        const availableSpots = maxSpots - (weekend.joinedUsers ? weekend.joinedUsers.length : 0);
+        return (
+          <div key={weekend.id} className="weekend-container" style={{ height: isWeekendBooked(weekend) ? "70px" : "normalHeight" }}>
+            <div className="header">
+              <h3>{weekend.dateRange}
+              {isWeekendBooked(weekend) && <span style={{ color: 'green' }}> (Booked)</span>}</h3>
+              {/* <p><span style={{ marginLeft: '20px', color: availableSpots > 0 ? 'blue' : 'red' }}>
+                {!isWeekendBooked(weekend) && availableSpots > 0 ? `${availableSpots-6} more to confirm booking` : ``}</span></p> */}
+              <div className="buttons">
+                <Button 
+                  variant="success" 
+                  className="join-btn" 
+                  onClick={() => handleJoin(weekend.id)}
+                  disabled={isWeekendBooked(weekend) || availableSpots === 0} // Check if weekend is booked or no spots are available
+                >
+                  Join
+                </Button>
+                <Button 
+                  variant="danger" 
+                  className="leave-btn" 
+                  onClick={() => handleLeave(weekend.id)}
+                  disabled={isWeekendBooked(weekend)} // Add this line
+                >
+                  Leave
+                </Button>
+              </div>
+            </div>
+            {!isWeekendBooked(weekend) && (
+              <>
+                <Row className="circle-row">
+                  {[...Array(6).keys()].map((index) => (
+                    <Col key={index} xs={2}>
+                      <Circle weekendId={weekend.id} index={index}/>
+                      </Col>
+                  ))}
+                </Row>
+                <Row className="circle-row">
+                  {[...Array(6).keys()].map((index) => (
+                    <Col key={index} xs={2}>
+                      <Circle weekendId={weekend.id} index={index + 6}/> {/* We added 6 to the index to correctly map to the second row of circles */}
+                    </Col>
+                  ))}
+                </Row>
+              </>
+            )}
           </div>
-        </div>
-        {!isWeekendBooked(weekend) && (
-          <>
-            <Row className="circle-row">
-              {[...Array(6).keys()].map((index) => (
-                <Col key={index} xs={2}>
-                  <Circle weekendId={weekend.id} index={index}/>
-                </Col>
-              ))}
-            </Row>
-            <Row className="circle-row">
-              {[...Array(6).keys()].map((index) => (
-                <Col key={index} xs={2}>
-                  <Circle />
-                </Col>
-              ))}
-            </Row>
-          </>
-        )}
-      </div>
-      
-      ))}
+        )
+      })}
     </Container>
   );
 };
